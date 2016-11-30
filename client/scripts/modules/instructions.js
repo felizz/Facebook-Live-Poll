03c4c2339a838532802857881db291ca5de1e5bd
knightus <coldkiller131@gmail.com>
2016-11-30 14:56:50 +0700

    Update HTML of poll page

--- client/scripts/modules/instructions.js
+++ client/scripts/modules/instructions.js
@@ -11,6 +11,66 @@ module.exports = function(sandbox){
                 _this.objects.$accordion.addClass('active');
             })
         ;
+
+        _this.bindFormVideoIdEvents();
+        _this.bindFormSetupStreamEvents();
+    }
+
+    _this.bindFormVideoIdEvents = () => {
+        _this.objects.$formVideoId
+            .on('submit', function(event){
+                event.preventDefault();
+                let pollId = _this.objects.$formVideoId.data('poll-id');
+
+                let requestedData = {
+                    fb_video_id: _this.objects.$formVideoId.find('[name=fb_video_id]').val()
+                };
+
+                $
+                    .ajax({
+                        type: 'POST',
+                        url: '/api/v1/poll/'+pollId+'/update-additional-info',
+                        data: JSON.stringify(requestedData),
+                        contentType: "application/json; charset=utf-8",
+                    })
+                    .done((response) => {
+                        console.log('response', response);
+                        if(response._id){
+                            window.location.reload();
+                        }
+                    })
+                ;
+            })
+        ;
+    }
+
+    _this.bindFormSetupStreamEvents = () => {
+        _this.objects.$formSetupStream
+            .on('submit', function(event){
+                event.preventDefault();
+                let pollId = _this.objects.$formSetupStream.data('poll-id');
+
+                let requestedData = {
+                    fb_stream_url: _this.objects.$formSetupStream.find('[name=fb_stream_url]').val(),
+                    fb_stream_key: _this.objects.$formSetupStream.find('[name=fb_stream_key]').val()
+                };
+
+                $
+                    .ajax({
+                        type: 'POST',
+                        url: '/api/v1/poll/'+pollId+'/update-additional-info',
+                        data: JSON.stringify(requestedData),
+                        contentType: "application/json; charset=utf-8",
+                    })
+                    .done((response) => {
+                        console.log('response', response);
+                        if(response._id){
+                            window.location.reload();
+                        }
+                    })
+                ;
+            })
+        ;
     }
 
     _this.init = (data) => {
@@ -18,6 +78,8 @@ module.exports = function(sandbox){
 
         _this.objects = {};
         _this.objects.$container = $('#segment_instructions');
+        _this.objects.$formVideoId = $('#form_video_id');
+        _this.objects.$formSetupStream = $('#form_setup_stream');
         _this.objects.$accordion = $('#accordion_instructions');
         _this.objects.$accordionHeader = _this.objects.$accordion.children('.header');
 
